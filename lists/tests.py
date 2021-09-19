59f9bac01872bb8d864247a43e29b6adb78b8783
zpy13 <azqmko@126.com>
2021-09-20 00:15:01 +0800

    make a migration

--- lists/tests.py
+++ lists/tests.py
@@ -5,7 +5,7 @@ from django.test.utils import require_jinja2
 
 
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item,List
 
 # Create your tests here.
 class HomePageTest(TestCase):
@@ -14,22 +14,33 @@ class HomePageTest(TestCase):
         response = self.client.get('/')       
         self.assertTemplateUsed(response,'home.html')
 
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = 'The First (ever) list item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(),2)
+
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text,'The First (ever) list item')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text,'Item the second')
+        self.assertEqual(second_saved_item.list, list_)
 
 class ListViewTest(TestCase):
     def test_uses_list_templates(self):
@@ -37,8 +48,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response,'list.html')
 
     def test_display_all_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1',list = list_)
+        Item.objects.create(text='itemey 2',list = list_)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
 
