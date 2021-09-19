b0e940ead0f7f93986ee46b83c8a43fb369ca5a3
zpy13 <azqmko@126.com>
2021-09-19 21:38:51 +0800

    add a new list

--- superlists/urls.py
+++ superlists/urls.py
@@ -20,7 +20,8 @@ from django.conf.urls import url
 
 urlpatterns = [
     #path('admin/', admin.site.urls),
-    url(r'^$',views.home_page,name = 'home'),
-    url(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list')
+    url(r'^$',views.home_page,name='home'),
+    url(r'^lists/new$',views.new_list,name='new_list'),
+    url(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list'),
 
 ]
