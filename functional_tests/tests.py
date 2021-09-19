90bc5442b6c9245b1b3a6c11a525b66a95e15e94
zpy13 <azqmko@126.com>
2021-09-19 16:29:08 +0800

    prepare to start a new list

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -24,7 +24,7 @@ class NewVisitorTest(LiveServerTestCase):
 					raise e
 				time.sleep(0.5)
 
-	def test_can_start_a_list_and_retrieve_it_later(self):
+	def test_can_start_a_list_and_for_one_user(self):
 		self.browser.get(self.live_server_url)
 		self.assertIn('To-Do',self.browser.title)
 
@@ -43,5 +43,37 @@ class NewVisitorTest(LiveServerTestCase):
 		inputbox.send_keys(Keys.ENTER)
 		self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
 		self.wait_for_row_in_list_table('1: Buy peacock feathers')
+	
+	def test_multiple_users_can_start_lists_at_different_urls(self):
+		self.browser.get(self.live_server_url)
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Buy peacock feathers')
+		inputbox.send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
-		self.fail('Finish the test!')
+		edith_list_url = self.browser.current_url
+		self.assertRegex(edith_list_url,'/lists/.+')
+
+		## start a new list and make sure edith's lists would not get leaked from cookie
+		self.browser.quit()
+		self.browser = webdriver.Firefox()
+
+		self.browser.get(self.live_server_url)
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Buy peacock feathers', page_text)
+		self.assertNotIn('make a fly',page_text)
+
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Buy milk')
+		inputbox.send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1: Buy milk')
+		
+		francis_list_url = self.browser.current_url
+		self.assertRegex(francis_list_url, '/lists/.+')
+		self.assertNotEqual(francis_list_url, edith_list_url)
+
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Buy peacock feathers', page_text)
+		self.assertIn('Buy milk',page_text)
+
+		
