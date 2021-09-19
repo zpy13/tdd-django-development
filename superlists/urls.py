1f0343d565a29086f1f64a44b1dc6d0c919a5be3
zpy13 <azqmko@126.com>
2021-09-19 19:55:25 +0800

    create a new list template

--- superlists/urls.py
+++ superlists/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls import url
 
 urlpatterns = [
     #path('admin/', admin.site.urls),
-    url(r'^$',views.home_page,name = 'home')
+    url(r'^$',views.home_page,name = 'home'),
+    url(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list')
 
 ]
