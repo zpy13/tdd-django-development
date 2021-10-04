e9ba0f95da12cba905e910176300c48bafaf7f4c
zpy13 <azqmko@126.com>
2021-10-04 17:08:55 +0800

    use 'wait for' to validate empty items

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,18 +1,23 @@
 from selenium.webdriver.common.keys import Keys
-from unittest import skip
 from .base import FunctionalTest
-
+import time
 
 class ItemValidationTest(FunctionalTest):
     def test_cannot_add_empty_list_items(self):
         # she sends an empty to-do item
         # press enter without any input in the input box
+        self.browser.get(self.live_server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
         # the page refreshes and sends a error message
         # "the to-do item can not be empty"
-
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            "You can't have an empty list item"
+        )
+        )
         # she enters something and submit, done!
-
+        self.fail("finish this test!")
         # she sends another empty to-do item
 
         # on the list page, she get the error message again
