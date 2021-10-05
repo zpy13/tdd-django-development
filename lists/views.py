7340151043f003250fb8ae9c504d817f69c5cc78
zpy13 <azqmko@126.com>
2021-10-05 09:54:42 +0800

    modify list view for validation

--- lists/views.py
+++ lists/views.py
@@ -1,20 +1,31 @@
 from django.shortcuts import redirect, render
-from lists.models import Item,List
+from lists.models import Item, List
+from django.core.exceptions import ValidationError
 
 # Create your views here.
 def home_page(request):
-    return render(request,'home.html')
+    return render(request, 'home.html')
 
-def view_list(request,list_id):
+
+def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    return render(request,'list.html',{'list':list_})
+    return render(request, 'list.html', {'list': list_})
+
 
 def new_list(request):
     list_ = List.objects.create()
-    Item.objects.create(text=request.POST['item_text'], list = list_)
+    item = Item.objects.create(text=request.POST['item_text'], list=list_)
+    try:
+        item.full_clean()
+        item.save()
+    except ValidationError:
+        list_.delete()
+        error = "You can't have an empty list item"
+        return render(request, 'home.html', {"error": error})
     return redirect(f'/lists/{list_.id}/')
 
-def add_item(request,list_id):
+
+def add_item(request, list_id):
     list_ = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST['item_text'],list = list_)
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect(f'/lists/{list_.id}/')
