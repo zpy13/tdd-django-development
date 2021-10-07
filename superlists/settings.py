1ec4fa88e0f1e901c264ebe309ac2de1943ab849
zpy13 <azqmko@126.com>
2021-10-07 19:21:59 +0800

    set DEBUG to false

--- superlists/settings.py
+++ superlists/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-rjyig1*o16bx@m+g2_&wt%-9wh6gjeqnl(&0ja%ft1^8j)=fev'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["yusutsu.xyz", "127.0.0.1"]
+ALLOWED_HOSTS = ["yusutsu.xyz"]
 
 
 # Application definition
