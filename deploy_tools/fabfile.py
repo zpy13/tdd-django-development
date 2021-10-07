ae9b40cee506423b0624a3a77893d32e03b7ce09
zpy13 <azqmko@126.com>
2021-10-07 16:34:44 +0800

    fixed typo

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -28,8 +28,8 @@ def _get_latest_source(source_folder):
         run(f'cd {source_folder} && sudo git fetch')
     else:
         run(f'git clone {REPO_URL} {source_folder}')
-    current_comment = local("git log -n 1 --format=%H", capture=True)
-    run(f'cd {source_folder} && git reset --hard {current_comment}')
+    current_commit = local("git log -n 1 --format=%H", capture=True)
+    run(f'cd {source_folder} && git reset --hard {current_commit}')
 
 
 def _update_settings(source_folder, site_name):
