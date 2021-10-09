16dc8c91615f292adc353f9f05748adf44af8ce3
zpy13 <azqmko@126.com>
2021-10-09 15:31:54 +0800

    add views, templates and test for views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect
+from django.core.mail import send_mail
+from django.contrib import messages
+from accounts.models import Token
+from django.urls import reverse
 # Create your views here.
+
+
+def send_login_email(request):
+    email = request.POST['email']
+    token = Token.objects.create(email=email)
+    url = request.build_absolute_uri(
+        reverse('login') + '?token=' + str(token.uid)
+    )
+    message_body = f'Use this link to log in:\n\n{url}'
+    send_mail(
+        'Your login link for Superlists',
+        message_body,
+        'noreply@superlists',
+        [email]
+    )
+    messages.success(
+        request,
+        "Check your email, we've sent you a link you can use to log in."
+    )
+    return redirect('/')
+
+
+def login(request):
+    return redirect('/')
